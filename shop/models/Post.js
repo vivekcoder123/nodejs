eb1edd115a291da8e33aa6c6423b4e0313db4b5b
Vivek Rautela <vivekrautela000@gmail.com>
2020-06-03 00:41:43 +0530

    merge

---
+++ shop/models/Post.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const URLSlugs = require('mongoose-url-slugs');
+
+const PostSchema = new Schema({
+    Category:{
+        type: Schema.Types.ObjectId,
+        ref:'categories'
+    },
+    user:{
+        type: Schema.Types.ObjectId,
+        ref:'users'
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    slug: {
+        type: String
+    },
+    status:{
+        type:String,
+        default:'public'
+    },
+    allowComments:{
+        type:Boolean,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+	file:{
+        type:String
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    },
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'comments'
+    }],
+    likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ]
+
+},{usePushEach: true});
+
+PostSchema.plugin(URLSlugs('title', {field: 'slug'}));
+
+module.exports = mongoose.model('posts',PostSchema);
